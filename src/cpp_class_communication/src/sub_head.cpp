de45a82841a54293d28093149d6882a9f35d8ee0
yepeichu123 <peichu.ye@gmail.com>
2020-02-24 21:10:16 +0800

    add publish

--- src/cpp_class_communication/src/sub_head.cpp
+++ src/cpp_class_communication/src/sub_head.cpp
@@ -2,6 +2,8 @@
 #include "std_msgs/Header.h"
 #include "std_msgs/ColorRGBA.h"
 #include "arbotix_msgs/Digital.h"
+#include <string>
+#include <sstream>
 
 void HeaderCallback(const std_msgs::Header::ConstPtr& head) {
     double t = head->stamp.toSec();
@@ -18,9 +20,32 @@ int main(int argc, char** argv) {
 
     // 发布example_ros中的节点需要的类型
     ros::Publisher pub_arbot = n.advertise<arbotix_msgs::Digital>("sub_arbot", 1000);
-    ros::Publisher pub_color = n.advertise<aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    ros::Publisher pub_color = n.advertise<std_msgs::ColorRGBA>("sub_color", 1000);
 
-    ros::spin();
+    int count = 1;
+    ros::Rate r(1);
+
+    while (ros::ok()) {
+
+        arbotix_msgs::Digital dgt;
+        if (count == 256) {
+            count = 0;
+        }
+        dgt.value = count++;
+        dgt.direction = count;
+        pub_arbot.publish(dgt);
+
+        std_msgs::ColorRGBA color;
+        color.r = 1 + 0.1 * count;
+        color.g = 10 + 0.1 * count;
+        color.b = 100 + 0.1 * count;
+        color.a = 0.5;
+        pub_color.publish(color);
+
+        ros::spinOnce();
+
+        r.sleep();
+    }
 
     return 0;
 }
